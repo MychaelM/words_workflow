8028650fc98c69d6d695fc91228c77653ea7d9f4
Mychael M <mychaelbmenges@gmail.com>
2025-02-12 20:31:25 -0700

    initial commit

---
+++ words_workflow.py
@@ -0,0 +1,18 @@
+import httpx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def print_words(words: list[str]):
+  for word in words:
+    word_dict = fetch_word(word)
+    definitions = word_dict['results'][0]['definition']
+    synonyms = word_dict['results'][0]['synonyms']
+    # print(word_dict['results'])
+    print(f'{definitions} {synonyms}')
+
+def fetch_word(word: str):
+  return httpx.get(f'https://wordsapiv1.p.rapidapi.com/words/{word}', headers={'x-rapidapi-key': os.getenv('RAPID_KEY'), 'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"}).json()
+
+print_words(['hello', 'apple'])
